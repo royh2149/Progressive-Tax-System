5b9a60ad7b21b100f84880050710c8deb874ba09
1234 <1234>
2021-09-29 18:49:27 +0300

    Fully Commented TaxLevel.py

--- TaxLevel.py
+++ TaxLevel.py
@@ -30,24 +30,38 @@ class TaxLevel:
 
         # open the csv file
         with open(tax_levels_filename, newline='') as source_file:
-            reader = csv.reader(source_file, delimiter=',')
+            reader = csv.reader(source_file, delimiter=',')  # the csv reader is used for a comfortable access to the csv columns
+            next(reader)  # skip the titles row
+            for row in reader:  # scan every level of taxation
+                min_sum = int(row[0])
+                max_sum = int(row[1])
+                level_percentage = float(row[2])
+
+                # after determining the different properties, create a new tax-level based on them and add it to the result list
+                result.append(TaxLevel(min_sum, max_sum, level_percentage))
+
+        return result
+
+# create the dictionaries holding the countries options and their tax levels
+codes = {1: "Israel"}
+levels_funcs = {1: TaxLevel.generate_israel_tax_levels()}
 
 
 def calculate_tax(to_pay: int, tax_levels: list) -> float:
-    tax = 0.0
-    already_calculated = 0
+    tax = 0.0  # the tax the user needs to pay
+    already_calculated = 0  # the amount of money for which the tax have already already calculated
     pay_sum = to_pay  # the total amount to pay
 
     for level in tax_levels:  # calculate the tax to be paid in each level
         if pay_sum < level.max_sum:  # check if the "last level" to pay was reached
             # print(str(float(to_pay)) + " * " + str(level.tax_percentage) + "% = " + str(float(to_pay) * (level.tax_percentage / 100.0)))
-            tax += float(to_pay) * (level.tax_percentage / 100.0)
-            break
+            tax += float(to_pay) * (level.tax_percentage / 100.0)  # add the tax in the current level to the sum
+            break  # since all the income have already been taxed, we can exit the loop
 
         # print(str(level.max_sum - already_calculated) + " * " + str(level.tax_percentage) + "% = " + str(float(level.max_sum - already_calculated) * (level.tax_percentage / 100.0)))
-        tax += float(level.max_sum - already_calculated) * (level.tax_percentage / 100.0)
+        tax += float(level.max_sum - already_calculated) * (level.tax_percentage / 100.0)  # add the tax in the current level to the sum
         to_pay -= (level.max_sum - already_calculated)  # update the amount of money left to be taxed
-        already_calculated += (level.max_sum - already_calculated)
+        already_calculated += (level.max_sum - already_calculated)  # update the amount of money that have already been taxed
 
-    return tax
+    return tax  # return the tax the user needs to pay
 
